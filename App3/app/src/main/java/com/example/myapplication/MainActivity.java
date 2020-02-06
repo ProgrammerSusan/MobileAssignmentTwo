a308b43ecbb0b1c70fb59e7f8937ae194f14301c
ProgrammerSusan <hatems2017@gmail.com>
2020-02-05 20:38:38 -0500

    added functionality to seek bars

--- App3/app/src/main/java/com/example/myapplication/MainActivity.java
+++ App3/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SeekBar red = (SeekBar) findViewById(R.id.redAmount);
+        SeekBar green = (SeekBar) findViewById(R.id.greenAmount);
+        SeekBar blue = (SeekBar) findViewById(R.id.blueAmount);
+
+        ColorPicker listener = new ColorPicker();
+
+        red.setOnSeekBarChangeListener(listener);
+        green.setOnSeekBarChangeListener(listener);
+        blue.setOnSeekBarChangeListener(listener);
     }
-}
+
+    private class ColorPicker implements SeekBar.OnSeekBarChangeListener {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            changeColor();
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        private void changeColor(){
+            TextView color = (TextView)findViewById(R.id.color);
+            SeekBar red = (SeekBar) findViewById(R.id.redAmount);
+            SeekBar green = (SeekBar) findViewById(R.id.greenAmount);
+            SeekBar blue = (SeekBar) findViewById(R.id.blueAmount);
+
+            int redValue = red.getProgress();
+            int greenValue = green.getProgress();
+            int blueValue = blue.getProgress();
+
+            //sets background color of large textbox
+            color.setBackgroundColor(Color.rgb(redValue, greenValue, blueValue));
+        }
+        }
+    }
