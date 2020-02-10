b6b1f5758c06f7e81937ed4f116c7c66aaa9fd0f
ProgrammerSusan <hatems2017@gmail.com>
2020-02-09 19:56:15 -0500

    added slider class

---
+++ App7/app/src/main/java/com/example/myapplication/Slide.java
@@ -0,0 +1,74 @@
+package cosc426.program;
+
+import java.util.Random;
+
+public class Slide
+{
+    private final char BLANK = ' ';
+    private Random random;
+
+    public Slide()
+    {
+        random = new Random();
+    }
+
+    public char[][] generateInitialBoard()
+    {
+        char[][] board = new char[3][3];
+
+        int[] list = createList();
+
+        int k = 0;
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+            {
+                if (list[k] == 0)
+                    board[i][j] = BLANK;
+                else
+                    board[i][j] = (char)('0' + list[k]);
+
+                k++;
+            }
+
+        return board;
+    }
+
+    public char[][] generateGoalBoard()
+    {
+        char[][] board = new char[3][3];
+
+        int k = 0;
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+            {
+                board[i][j] = (char)('0' + k + 1);
+                k++;
+            }
+
+        board[2][2] = BLANK;
+
+        return board;
+    }
+
+    private int[] createList()
+    {
+        int[] list = new int[9];
+
+        for (int i = 0; i < 9; i++)
+            list[i] = i;
+
+        int k = 8;
+        for (int i = 0; i < 9; i++)
+        {
+            int j = random.nextInt(k+1);
+
+            int temp = list[j];
+            list[j] = list[k];
+            list[k] = temp;
+
+            k = k - 1;
+        }
+
+        return list;
+    }
+}
